a5e8a285ffc7caf9261a836975a9bed3a172c379
duboisst <stefane.dubois@free.fr>
2018-02-07 23:14:38 +0100

    fichier manquant

---
+++ src/app/joueur.ts
@@ -0,0 +1,4 @@
+export class Joueur {
+    constructor(public _id:number, public licence:string, public prenom:string, public nom:string, public club:string, public classement:number, categorie:string) {
+    }
+}
